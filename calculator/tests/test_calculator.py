98d41f287be934b007ba7f243511dd507cee10b9
Ricardo <riki.1997.121@gmail.com>
2024-06-03 10:32:27 +0100

    Agregar prueba para la raiz cuadrada

--- calculator/tests/test_calculator.py
+++ calculator/tests/test_calculator.py
@@ -19,3 +19,7 @@ def test_division():
     calc = Calculadora()
     assert calc.dividir(6, 2) == 3
 
+def test_square_root():
+    calc = Calculadora()
+    assert calc.sqrt(9) == pytest.approx(3, rel=1e-3)
+    assert calc.sqrt(2) == pytest.approx(1.414, rel=1e-3)
