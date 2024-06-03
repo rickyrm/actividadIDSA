a64357e10ccf21b1c8e4958a6fce2e3bd4103c34
Ricardo <riki.1997.121@gmail.com>
2024-06-03 10:21:10 +0100

    Agregar pruebas iniciales para la suma y resta

--- calculator/src/calculator.py
+++ calculator/src/calculator.py
@@ -1,31 +1,4 @@
-# src/calculator.py
-class Calculadora:
-    def sumar(self, a, b):
-        return a + b
 
-    def restar(self, a, b):
-        return a - b
-
-    def multiplicar(self, a, b):
-        return a * b
-
-    def dividir(self, a, b):
-        if b == 0:
-            raise ValueError("No se puede dividir entre cero")
-        return a / b
-
-    def babylonian_sqrt(self, n, precision=0.001):
-        if n < 0:
-            raise ValueError("No se puede sacar la raiz cuadrada de un numero negativo")
-        x = n
-        y = (x + n / x) / 2
-        while abs(x - y) > precision:
-            x = y
-            y = (x + n / x) / 2
-        return y
-
-    def sqrt(self, n):
-        return self.babylonian_sqrt(n)
 
 
 
